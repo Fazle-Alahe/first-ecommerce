42e95450b9c5caf50bab847cbf80f971e2b66461
fazlealahe <fazlealahe.murad@gmail.com>
2023-10-11 17:26:40 +0600

    products insert

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -124,11 +124,21 @@
                     <div class="col-lg-3 col-md-4 col-sm-6 col-12">
                         <div class="product-item">
                             <div class="image">
-                                <img height="250" src="{{asset('uploads/product/preview/')}}/{{$product->prev_img}}" alt="">
-                                <div class="tag new">New</div>
+                                <img height="250px" src="{{asset('uploads/product/preview/')}}/{{$product->prev_img}}" alt="">
+                                @if ($product->discount)
+                                    <div class="tag sale">-{{$product->discount}}%</div>
+                                @else
+                                    <div class="tag new">New</div>
+                                @endif
                             </div>
                             <div class="text">
-                                <h2><a href="product-single.html">{{$product->product_name}}</a></h2>
+                                <h2><a href="product-single.html" title="{{$product->product_name}}">
+                                    @if (strlen($product->product_name) > 20)
+                                        {{substr($product->product_name, 0, 20).'...'}}
+                                    @else
+                                        {{$product->product_name}}
+                                    @endif
+                                    </a></h2>
                                 <div class="rating-product">
                                     <i class="fi flaticon-star"></i>
                                     <i class="fi flaticon-star"></i>
@@ -138,8 +148,10 @@
                                     <span>130</span>
                                 </div>
                                 <div class="price">
-                                    <span class="present-price">$120.00</span>
-                                    <del class="old-price">$200.00</del>
+                                    <span class="present-price">&#2547; {{$product->after_discount}}</span>
+                                    @if ($product->discount)
+                                        <del class="old-price">&#2547; {{$product->product_price}}</del>
+                                    @endif
                                 </div>
                                 <div class="shop-btn">
                                     <a class="theme-btn-s2" href="product.html">Shop Now</a>
